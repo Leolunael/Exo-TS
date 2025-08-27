a94b4fb678d4f144570297c02acdb609b3aaf02f
leolunael <leolunael@gmail.com>
2025-08-27 12:17:12 +0200

    exo 3,4,5

--- src/index.ts
+++ src/index.ts
@@ -23,8 +23,8 @@ console.log("Total panier", totalPanier(5,85,16,7,6));
 // Cela ne fonctionne pas pour moi, mais je n'ai peut-être pas la bonne façon de faire.
 
 // EXERCICE 2
-function saluer(prenom = "Anonyme"): string {
-    return '${prenom}';
+function saluer(prenom: string = "Anonyme"): string {
+    return `${prenom}`;
 }
 console.log("Bonjour", saluer(),"!");
 console.log("Bonjour", saluer("toto"),"!");
@@ -34,12 +34,12 @@ console.log("Bonjour", saluer("toto"),"!");
 // et non Anonyme même quand je mets "toto"
 
 function prixTTC(prixHT: number, tauxTVA: number = 0.2): number {
-    return prixHT+(prixHT*tauxTVA);
+    return prixHT+(prixHT*tauxTVA); // on peut rajouter .toFixed(chiffre après la virgule) ex .toFixed(2) = 1.11 
 }
 console.log("Prix TTC:", prixTTC(10));
 
 function ajouterPourboire(total: number, tip?: number): number {
-    const pourboire = tip !== undefined ? tip : 0;
+    const pourboire = tip !== undefined ? tip : 0; //const pourboire = tip ?? 0
     return total + pourboire;
 }
 console.log("Total sans pourboire:", ajouterPourboire(50, 0));
@@ -52,3 +52,38 @@ function formatContact(opts: { nom?: string; ville?: string }): string {
 }
 console.log("Contact :", formatContact({ nom: "Toto", ville: "Lyon" }));
 console.log("Contact :", formatContact({nom: "Toto"}));
+
+// EXERCICE 3
+
+const arrondirAuCentime = function (n: number): number { 
+    return Math.round(n * 100) / 100;
+}
+console.log("Arrondir à 2 décimal:", arrondirAuCentime(3.141592653589793));
+
+const appliquerReduction = (prix: number): number => prix * 0.5;
+console.log("Prix 50€ avec réduction:", appliquerReduction(50));
+
+const upperProduits = (liste: string[]): string[] => liste.map(produit => produit.toUpperCase());
+const list = ["panier", "math", "kilometre", "centimes"]
+console.log("Liste uppercase:", upperProduits(list));
+
+    //Exo 3 4ème question creerContact => pas compris comment faire
+
+// EXERCICE 4
+// Je n'ai pas compris ce qui fallait faire
+
+// EXERCICE 5
+
+function logAction(message: string): void{
+    console.log(`${message}`);
+}
+logAction("Courrir");
+
+function erreurFatale(message: string): never {
+    throw new Error(message);
+}
+
+async function getMeteo(ville: string): Promise<{ ville: string; degres: number }>{
+    return Promise.resolve({ ville, degres: 21 });
+}
+console.log("ville de", getMeteo("Lyon"));
